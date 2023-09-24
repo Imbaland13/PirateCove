32f2f7287be731a04009df02bcedd7ec58c20df8
Fred <fmaxgalaxy@gmail.com>
2023-09-24 22:56:32 +0200

    Test commit

--- HashTable/Manager.cs
+++ HashTable/Manager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// TODO: TEST COMMENT FOR PR
 namespace PiratesCoves
 {
     internal class Manager
